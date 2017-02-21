ea833eb40293344fda1d00463f585c82b02c12f2
jbriscoe <jbriscoe@pureheart.org>
2017-02-21 12:15:36 -0700

    Created test forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  ## Get data from user
+
+
+  ## Process options
+
+  ## Create accounts based on options
 end
