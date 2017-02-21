4f68c81d26b6fb41d4f7fa42ae13a8fb9c9f2bd5
jbriscoe <jbriscoe@pureheart.org>
2017-02-21 13:23:45 -0700

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'user_form/user_form'
+  get 'user_form/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
