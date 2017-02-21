b49f81bd8ec29d851abd62f7783e1a8706aab549
jbriscoe <jbriscoe@pureheart.org>
2017-02-21 13:32:49 -0700

    Routes troubleshooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'user_form/index'
+  get 'index' => "UserFormController#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
