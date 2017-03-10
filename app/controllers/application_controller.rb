738c30713331c2da287cb95227326bbf411e6e54
jbriscoe <jbriscoe@pureheart.org>
2017-03-10 15:16:16 -0700

    Updated Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+
+#Setup for Active Directory Authentication
+  protect_from_forgery
+      helper_method :current_user
+      def current_user
+          @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+      def authenticate_user!
+          if current_user.nil?
+            redirect_to '/sessions/new', :error => "Invalid Login"
+          end
+    end
+
+
+  #protect_from_forgery with: :exception
+
+
 
   ## Get data from user
 
